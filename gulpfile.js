a38c46e9a844c452f27f6c865360d139d638cea4
Tom Genoni <tom@atomeye.com>
2017-03-03 19:45:10 -0700

    Adding stuff

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var jsonImporter = require('node-sass-json-importer')
+
+gulp.task("sass", function () {
+  return gulp.src("foo.scss")
+    .pipe(sass({
+      importer: [jsonImporter]
+    }))
+    .pipe(gulp.dest('./'))
+});
